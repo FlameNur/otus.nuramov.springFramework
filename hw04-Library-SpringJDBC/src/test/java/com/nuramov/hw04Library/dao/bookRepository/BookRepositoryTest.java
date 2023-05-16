067c03e694e836b79a13a150c6ded36b1cc5086b
nuramoff <nuramoff@gmail.com>
2023-05-16 10:57:04 +0300

    Версия 1.17

--- hw04-Library-SpringJDBC/src/test/java/com/nuramov/hw04Library/dao/bookRepository/BookRepositoryTest.java
+++ hw04-Library-SpringJDBC/src/test/java/com/nuramov/hw04Library/dao/bookRepository/BookRepositoryTest.java
@@ -8,15 +8,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.test.context.jdbc.Sql;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @JdbcTest
-//@Sql({"/data.sql", "/schema.sql"})
 public class BookRepositoryTest {
+
+    private final Long ID = 10L;
 
     @Autowired
     private NamedParameterJdbcOperations jdbcOperations;
@@ -29,40 +31,115 @@ public class BookRepositoryTest {
     }
 
     @Test
-    void CountTest() {
+    void countTest() {
         assertEquals(3, bookRepository.count());
     }
 
     @Test
-    void SaveTest() {
-        Book savedBook = getBook();
+    void saveTest() {
+        Book newBook = getBook();
 
-        bookRepository.save(savedBook);
+        bookRepository.save(newBook);
 
-        Book bookFromDataBase = null;
-        Optional<Book> optionalBook = bookRepository.findById(10L);
-        if(optionalBook.isPresent()) {
-            bookFromDataBase = optionalBook.get();
-        }
+        Book bookFromDataBase = getBookFromDataBase(ID);
 
         assertNotNull(bookFromDataBase);
-        assertEquals(savedBook, bookFromDataBase);
+        assertEquals(newBook, bookFromDataBase);
+        assertEquals(4, bookRepository.count());
+    }
+
+    @Test
+    void updateTest() {
+        Book newBook = getBook();
+
+        bookRepository.save(newBook);
+
+        newBook.setTitle("newUpdatedTitle");
+        newBook.setAuthor(getNewAuthorName());
+        newBook.setGenre(getNewGenreName());
+
+        bookRepository.update(newBook);
+
+        Book bookFromDataBase = getBookFromDataBase(ID);
+
+        assertNotNull(bookFromDataBase);
+        assertEquals(newBook, bookFromDataBase);
+        assertEquals(4, bookRepository.count());
+    }
+
+    @Test
+    void deleteByIdTest() {
+        assertEquals(3, bookRepository.count());
+
+        bookRepository.deleteById(2L);
+
+        Book bookFromDataBase = getBookFromDataBase(2L);
+
+        assertEquals(2, bookRepository.count());
+        assertNull(bookFromDataBase);
+    }
+
+    @Test
+    void findAllAndFindByIDTest() {
+        assertEquals(3, bookRepository.count());
+
+        Book book1 = getBookFromDataBase(1L);
+        Book book2 = getBookFromDataBase(2L);
+        Book book3 = getBookFromDataBase(3L);
+
+        Book[] arrayOfBooks = new Book[3];
+        arrayOfBooks[0] = book1;
+        arrayOfBooks[1] = book2;
+        arrayOfBooks[2] = book3;
+
+        Book[] arrayOfBooksFromDataBase = new Book[3];
+        List<Book> listOfBooksFromDataBase = bookRepository.findAll();
+        int i = 0;
+        for(Book book : listOfBooksFromDataBase) {
+            arrayOfBooksFromDataBase[i] = book;
+            i++;
+        }
+
+        assertArrayEquals(arrayOfBooks, arrayOfBooksFromDataBase);
     }
 
     private Book getBook() {
         Genre genre = new Genre();
-        genre.setId(10L);
+        genre.setId(ID);
         genre.setName("genreNameTest");
 
         Author author = new Author();
-        author.setId(10L);
+        author.setId(ID);
         author.setName("authorNameTest");
 
         Book book = new Book();
-        book.setId(10L);
+        book.setId(ID);
         book.setTitle("bookTitleTest");
         book.setGenre(genre);
         book.setAuthor(author);
         return book;
+    }
+
+    private Book getBookFromDataBase(Long id) {
+        Book bookFromDataBase = null;
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            bookFromDataBase = optionalBook.get();
+        }
+        return bookFromDataBase;
+    }
+
+    private Author getNewAuthorName() {
+        Author author = new Author();
+        author.setId(ID);
+        author.setName("newAuthorNameTest");
+        return author;
+    }
+
+    private Genre getNewGenreName() {
+        Genre genre = new Genre();
+        genre.setId(ID);
+        genre.setName("newGenreNameTest");
+        return genre;
     }
 }
